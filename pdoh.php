9b43ac146afb186b59431c51ff8a351727d9fb38
galfrano <galfrano@gmail.com>
2017-05-22 20:09:42 +0200

    Update pdoh.php

--- pdoh.php
+++ pdoh.php
@@ -1,43 +1,47 @@
 <?php /*by galfrano@gmail.com*/
-/*the role of this class is solely to provide interactions with database adapter and statements. database agnostic.*/
-abstract class pdoh{
+//the role of this class is solely to provide interactions with database adapter and statements. database agnostic.
+namespace Model;
+class Pdoh{
 
 	//TODO: create and import some constants for readability
 
-	protected $dbi, $stmt = [];
+	protected $dbi, $stmt = [], $cstmt = -1;
 
 	protected static $pdo = [];
 
 	function __construct($dbi, $pdo){
-		key_exists($dbi, self::$pdo) ?: ($pdo instanceof PDO ? self::$pdo[$dbi] = $pdo : self::kill('Second parameter must be PDO (or first parameter must be a previously assigned index).'));
+		key_exists($dbi, self::$pdo) ?: ($pdo instanceof \PDO ? self::$pdo[$dbi] = $pdo : self::kill('Second parameter must be PDO (or first parameter must be a previously assigned index).'));
 		$this->dbi = $dbi;}
 
-	protected static function kill($stmt, $tokens = []){//throws exception
-		$message = $stmt instanceof PDOStatement ? $stmt->errorInfo()[2]."\n\n\t".'Query string: '.$stmt->queryString."\n\t".'Tokens: '.str_replace("\n", '', var_export($tokens, 1)) : $stmt ;
+	//throws exception
+	protected static function kill($stmt, $tokens = []){
+		$message = $stmt instanceof \PDOStatement ? $stmt->errorInfo()[2]."\n\n\t".'Query string: '.$stmt->queryString."\n\t".'Tokens: '.str_replace("\n", '', var_export($tokens, 1)) : $stmt ;
 		throw new \Exception($message."\n\n");}
 
-	function smdbg(){
-		var_export(get_object_vars(self::$pdo[$this->dbi]));
-		return $this;}
-
-	function query($query, $tokens = []){//prepares query, optionaly executes it
+	//prepares query, executes it and returns this or returns lamda-set-statement/this.
+	//use 1: $table = $pdoh->query($query, $tokens)->fetchAll();
+	//use 2: $stmt = $pdoh->query($query, false); $stmt()->execute($tokens)->fetchAll();
+	function query($query, $tokens = []){
 		$this->stmt[] = self::$pdo[$this->dbi]->prepare($query);
-		is_bool($tokens) ?: end($this->stmt)->execute((is_array($tokens) ? $tokens : [$tokens])) or self::kill(end($this->stmt), $tokens);
-		return $this;}
+		($bool = is_bool($tokens)) ? list($c, $this->cstmt) = [count($this->stmt)-1, -1] : end($this->stmt)->execute((is_array($tokens) ? $tokens : [$tokens])) or self::kill(end($this->stmt), $tokens);
+		return !$bool ? $this : function()use($c){
+			$this->cstmt = $c;
+			return $this;};}
 
 	function id(){
 		return self::$pdo[$this->dbi]->lastInsertId();}
 
 	function execute($tokens = [], $stmt = -1){
 		$this->valid($stmt);
-		return $this->stmt[$stmt]->execute($tokens);}
+		$this->stmt[$stmt]->execute($tokens) or self::kill($this->stmt[$stmt], $tokens);
+		return $this;}
 
 	function fetchAll($callback = false, $stmt = -1, $fs = 2){
 		$this->valid($stmt);
 		if($c = is_callable($callback)){
-			for($r = []; $line = $this->stmt[$stmt]->fetch($fs); $cr = $callback($line, $r), !is_array($cr) ?: $r += $cr);}//TODO:callback pass by reference
+			for($r = []; $line = $this->stmt[$stmt]->fetch($fs); $cr = $callback($line), !is_array($cr) ? $r[] = $line : $r += $cr);}
 		return $c ? $r : $this->stmt[$stmt]->fetchAll($fs);}
 
 	protected function valid(&$stmt){
-		$stmt = $stmt < 0 ? count($this->stmt)-1 : $stmt ;
-		$stmt >= 0 or die('No statements');}}
+		$stmt = $stmt < 0 ? ($this->cstmt < 0 ? count($this->stmt)-1 : $this->cstmt) : $stmt ;
+		$stmt >= 0 && key_exists($stmt, $this->stmt) or die('No such statement');}}
